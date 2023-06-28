09c86578a166877f8460f4818d494065bcd80e5f
Parsa20231401 <s.a.parsahosseini@gmail.com>
2023-06-28 18:47:44 +0430

    Revert "Update ui_server.h"
    This reverts commit c5bc35878c920a944ac79ace06bc3665503a85b9.

--- entername2/ui_server.h
+++ entername2/ui_server.h
@@ -12,33 +12,33 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QComboBox>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
-#include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
 
-class Ui_chatpage
+class Ui_server
 {
 public:
     QWidget *centralwidget;
     QPushButton *pushButton;
     QLineEdit *lineEdit;
+    QLabel *textBrowser_receivedMessages;
     QComboBox *comboBox_receiver;
-    QTextBrowser *textBrowser;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
-    void setupUi(QMainWindow *chatpage)
+    void setupUi(QMainWindow *server)
     {
-        if (chatpage->objectName().isEmpty())
-            chatpage->setObjectName(QString::fromUtf8("chatpage"));
-        chatpage->resize(889, 591);
-        centralwidget = new QWidget(chatpage);
+        if (server->objectName().isEmpty())
+            server->setObjectName(QString::fromUtf8("server"));
+        server->resize(889, 591);
+        centralwidget = new QWidget(server);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
@@ -46,38 +46,40 @@ public:
         lineEdit = new QLineEdit(centralwidget);
         lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
         lineEdit->setGeometry(QRect(0, 480, 641, 31));
+        textBrowser_receivedMessages = new QLabel(centralwidget);
+        textBrowser_receivedMessages->setObjectName(QString::fromUtf8("textBrowser_receivedMessages"));
+        textBrowser_receivedMessages->setGeometry(QRect(0, 0, 731, 481));
+        textBrowser_receivedMessages->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 85, 0);"));
         comboBox_receiver = new QComboBox(centralwidget);
         comboBox_receiver->setObjectName(QString::fromUtf8("comboBox_receiver"));
         comboBox_receiver->setGeometry(QRect(480, 430, 181, 31));
-        textBrowser = new QTextBrowser(centralwidget);
-        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
-        textBrowser->setGeometry(QRect(50, 120, 501, 261));
-        chatpage->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(chatpage);
+        server->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(server);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 889, 25));
-        chatpage->setMenuBar(menubar);
-        statusbar = new QStatusBar(chatpage);
+        menubar->setGeometry(QRect(0, 0, 889, 26));
+        server->setMenuBar(menubar);
+        statusbar = new QStatusBar(server);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
-        chatpage->setStatusBar(statusbar);
+        server->setStatusBar(statusbar);
 
-        retranslateUi(chatpage);
+        retranslateUi(server);
 
-        QMetaObject::connectSlotsByName(chatpage);
+        QMetaObject::connectSlotsByName(server);
     } // setupUi
 
-    void retranslateUi(QMainWindow *chatpage)
+    void retranslateUi(QMainWindow *server)
     {
-        chatpage->setWindowTitle(QApplication::translate("chatpage", "MainWindow", nullptr));
-        pushButton->setText(QApplication::translate("chatpage", "PushButton", nullptr));
+        server->setWindowTitle(QApplication::translate("server", "MainWindow", nullptr));
+        pushButton->setText(QApplication::translate("server", "PushButton", nullptr));
+        textBrowser_receivedMessages->setText(QApplication::translate("server", "TextLabel", nullptr));
     } // retranslateUi
 
 };
 
 namespace Ui {
-    class chatpage: public Ui_chatpage {};
+    class server: public Ui_server {};
 } // namespace Ui
 
 QT_END_NAMESPACE
 
-#endif // UI_SERVER_H
+#endif // UI_server_H
